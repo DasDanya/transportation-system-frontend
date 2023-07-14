7ff183aeac481a4c8dfbc315275d91fbdd26dec6
Vadim Gureev <vdm_g@mail.ru>
2023-07-14 10:59:29 +0300

    Authorization

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'transportation-system-frontend'`, () => {
+  it(`should have as title 'demofront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('transportation-system-frontend');
+    expect(app.title).toEqual('demofront');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('transportation-system-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demofront app is running!');
   });
 });
